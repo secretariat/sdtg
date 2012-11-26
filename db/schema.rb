c3be5da9052425b65bf89ce0ff0f92a93139daf5
Andrew Golovin <golovin.010@gmail.com>
2012-11-27 00:55:06 +0200

    index.html+++

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,12 +12,6 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20121126213427) do
-
-  create_table "transports", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
   create_table "zapros", :force => true do |t|
     t.string   "naim"
