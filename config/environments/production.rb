fc53768c0dcc4ce67ffbd39b40c07e475fa45d05
Secretariat1987 <mixey.viti@bigmir.net>
2012-12-04 00:55:54 +0200

    20121204_send_zapros_with_attach

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Sdtg::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
