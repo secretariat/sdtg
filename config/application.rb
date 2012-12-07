d03f36de3fcff8de090801a5f12f8eacde70f682
Secretariat1987 <mixey.viti@bigmir.net>
2012-12-07 14:37:11 +0200

    20121207_text_fixes

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,8 @@ module Sdtg
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
+    # config.assets.paths << Rails.root.join("app", "assets", "videos")
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
