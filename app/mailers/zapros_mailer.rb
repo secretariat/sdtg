f76959e1a847ef4abf698bc96803870f7d4052e9
Secretariat1987 <mixey.viti@bigmir.net>
2013-07-18 16:41:43 +0300

    add validation and email changing

--- app/mailers/zapros_mailer.rb
+++ app/mailers/zapros_mailer.rb
@@ -4,7 +4,7 @@ class ZaprosMailer < ActionMailer::Base
   # default to: "mixey.viti@bigmir.net"
 
 
-  def send_zapros( zapros, fpath, fname )
+  def send_zapros( zapros, fpath, fname, email_to )
     pdf = ZaprosPdf.new( zapros )
     if( fname != nil ) then
     	att_name = Rails.root.join("public#{fpath}")
@@ -16,7 +16,7 @@ class ZaprosMailer < ActionMailer::Base
     attachments["zapros_#{zapros.id}.pdf"] = { :mime_type => "application/pdf", :content => pdf.render }
 
     @zapros = zapros
-    mail( :from => @zapros.email, :subject =>"Запрос на определение стоимости финансовой гарантии")
+    mail( :to => email_to, :from => @zapros.email, :subject =>"Запрос на определение стоимости финансовой гарантии")
    	if( fd ) then
       fd.close
     end
--- app/models/zapros.rb
+++ app/models/zapros.rb
@@ -27,4 +27,11 @@ class Zapros < ActiveRecord::Base
                     :company_name,
                     :dog_num,
                     :dog_date
-    end
+
+    # validates_presence_of :naim, :message => "Поле не может быть пустым"
+    validates_presence_of :code_num, :message => "Поле не может быть пустым"
+    validates_presence_of :email, :message => "Поле не может быть пустым"
+    validates_presence_of :fio, :message => "Поле не может быть пустым"
+    validates_presence_of :rank, :message => "Поле не может быть пустым"
+    validates_presence_of :phone, :message => "Поле не может быть пустым"
+end
